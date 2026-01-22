2f9638c76c49627db89cdf76bc1c1dbb862362a3
roboatLee <1057526780@qq.com>
2026-01-22 13:15:33 +0800

    新学习props内容

---
+++ wecomtest/src/page/StudyPage/Child.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>{{ msg }}</div>
+  <button @click="changeMsg">修改父组件数据</button>
+</template>
+
+<script setup>
+const props = defineProps({
+  msg: String
+})
+
+const emit = defineEmits(['change'])
+
+function changeMsg() {
+  emit('change', '这是子组件发回去的新值')
+}
+</script>
---
+++ wecomtest/src/page/StudyPage/Parent.vue
@@ -0,0 +1,10 @@
+<template>
+  <Child :msg="parentMsg" @change="parentMsg = $event" />
+</template>
+
+<script setup>
+import Child from './Child.vue'
+import { ref } from 'vue'
+
+const parentMsg = ref('原始内容')
+</script>
